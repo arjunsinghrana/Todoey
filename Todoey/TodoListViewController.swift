4da4047aedc3f383d43c3a5995d16fcfaa0618ee
Arjun <asingh105@jaguarlandrover.com>
2018-07-16 11:32:30 +0100

    Implement persistent storage using UserDefaults

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -12,9 +12,15 @@ class TodoListViewController: UITableViewController {
 
     var itemArray: [String] = ["Hello", "Bye"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        if let items = defaults.array(forKey: "itemArray") as? [String] {
+            itemArray = items
+        }
     }
 
 
@@ -67,6 +73,8 @@ class TodoListViewController: UITableViewController {
             if let text = textField.text {
                 self.itemArray.append(text)
                 self.tableView.reloadData()
+                
+                self.defaults.set(self.itemArray, forKey: "itemArray")
             }
             
         }
